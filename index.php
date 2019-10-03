af6322550ee35e381f2f7a0e72e1959b7f77bec8
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2019-10-03 22:00:36 +0700

    memperbaiki kesalahan syntax

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo '<title>Selamat Datang | UHA</title>'
-echo '<h3><center>Hallo, UHA-Developer!</center></p>';
-echo '<p><center>masih dalam tahap perkembangan</center></p>';
+echo '<head><title>Selamat Datang | UHA</title></head>'
+echo '<body><h3><center>Hallo, UHA-Developer!</center></p>';
+echo '<p><center>masih dalam tahap perkembangan</center></p></body>';
