409f2cebd1e53f70b768bdb16e90d5afdbf1f0b0
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2019-10-03 22:34:14 +0700

    memperbaiki kurang ; di commit sebelumnya

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo '<head><title>Selamat Datang | UHA</title></head>'
+echo '<head><title>Selamat Datang | UHA</title></head>';
 echo '<body><h3><center>Hallo, UHA-Developer!</center></p>';
 echo '<p><center>masih dalam tahap perkembangan</center></p></body>';
