be367e8509faf748bb27cbc8009c147244aca294
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2019-10-04 16:35:26 +0700

    perbaruan setelah push

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
 echo '<head><title>Selamat Datang | UHA</title></head>';
-echo '<body><h3><center>Hallo, UHA-Developer!</center></p>';
-echo '<p><center>masih dalam tahap perkembangan</center></p></body>';
+echo '<link rel="icon" href="https://farridkuntoro.me/favicon.ico">';
+echo '<body><h3><center>Hallo, UHA-Developer!</center></h3>';
+echo '<hr>';
+echo '<p><center>masih dalam tahap perkembangan</center></p>';
+echo '<p><center>harap menunggu</center></p></body>';
+?>
