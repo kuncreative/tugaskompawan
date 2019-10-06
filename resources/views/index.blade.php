19f7975eb3f45fe06886cd5142c70d349b1c249a
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2019-10-06 15:26:30 +0700

    Penerapan Landing Page 70%

---
+++ resources/views/index.blade.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+        <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+              
+                <title>Selamat Datang | UHA-Developer</title>
+              
+                <!-- Favicon -->
+                <link rel="shortcut icon" href="http://farridkuntoro.me/favicon.ico">
+              
+                <!-- Main CSS file / CSS Utama -->
+                <link rel="stylesheet" href="/css/style.css">
+              
+              </head>
+              
+              <body>
+              
+                <!-- PRELOADER -->
+                <div class="ds-preloader"></div>
+                <!-- /PRELOADER -->
+              
+                <!-- HEADER -->
+                <header>
+              
+                  <!-- HEADER OVERLAY -->
+                  <div class="ds-overlay ds-overlay-color"></div>
+                  <!-- /HEADER OVERLAY -->
+              
+                  <div class="container">
+              
+                    <!-- LOGO & SOCIALS -->
+                    <div class="row">
+                      <!-- LOGO -->
+                      <div class="col-sm-4 ds-logo">
+                        <a href="#" class="ds-button-left">UHA-Developer</a>
+                      </div>
+                      <!-- /LOGO -->
+                      <!-- TEXT + BUTTON -->
+                      <div class="col-sm-8 ds-text-button">
+                        <a href="#pricing">LOGIN UHA</a>
+                      </div>
+                      <!-- /TEXT + BUTTON -->
+                    </div>
+                    <!-- /LOGO & SOCIALS -->
+              
+                    <!-- TITLE -->
+                    <div class="row">
+                      <div class="col-xs-12 col-md-7 ds-title">
+                        <h1>UHA-Developer <br> kini telah hadir! </h1>
+                      </div>
+                    </div>
+                    <!-- /TITLE -->
+              
+                    <!-- TITLE PARAGRAPH -->
+                    <div class="row">
+                      <div class="col-sm-12 col-md-5 ds-title-paragraph">
+                        <p> Berdasarkan semangat juang koding yang tinggi untuk mendapatkan pundi-pundi ilmu
+                          yang sangat berguna bagi secercah pencari cuan di lain hari.</p>
+                      </div>
+                    </div>
+                    <!-- /TITLE PARAGRAPH -->
+              
+                    <!-- BUTTONS -->
+                    <div class="row">
+                      <div class="col-xs-12 col-sm-8 ds-buttons">
+                        <a href="mailto:hai@farridkuntoro.me" class="ds-button-left">LEBIH TAU UHA <i
+                            class="fa fa-long-arrow-right"></i></a><a href="https://github.com/kuncreative/tugaskompawan"
+                          class="ds-button-right">REPO GITHUB</a>
+                      </div>
+                    </div>
+                    <!-- /BUTTONS -->
+              
+                    <!-- SCROLL DOWN -->
+                    <div class="row">
+                      <div class="col-sm-12 ds-scroll-down">
+                        <a href="#" title=""><i class="fa fa-chevron-down"></i></a>
+                      </div>
+                    </div>
+                    <!-- /SCROLL DOWN -->
+              
+                  </div>
+              
+                </header>
+                <!-- /HEADER -->
+              
+                <!-- MADE BY -->
+                <div class="clients module">
+                  <div class="container">
+                    <!-- TITLE -->
+                    <div class="row">
+                      <div class="col-sm-8 col-sm-offset-2 ds-clients-title">
+                        <h2>– DIBUAT MENGGUNAKAN & MELALUI –</h2>
+                      </div>
+                    </div>
+                    <!-- /TITLE -->
+                    <!-- COLUMNS -->
+                    <div class="row">
+                      <div class="col-sm-12 ds-clients">
+                        <div class="ds-clients-carousel">
+                          <!-- FIRST ROW -->
+                          <div class="row">
+                            <div class="col-xs-6 col-sm-2 ds-clients-item">
+                              <img src="/img/logo-heroku.png" alt="">
+                            </div>
+                            <div class="col-xs-6 col-sm-2 ds-clients-item">
+                              <img src="/img/logo-laravel.png" alt="">
+                            </div>
+                            <div class="col-xs-6 col-sm-2 ds-clients-item">
+                              <img src="/img/logo-pgsql.png" alt="">
+                            </div>
+                            <div class="col-xs-6 col-sm-2 ds-clients-item">
+                              <img src="/img/logo-gitbash.png" alt="">
+                            </div>
+                            <div class="col-xs-6 col-sm-2 ds-clients-item">
+                              <img src="/img/logo-mscode.png" alt="">
+                            </div>
+                            <div class="col-xs-6 col-sm-2 ds-clients-item">
+                              <img src="/img/logo-composer.png" alt="">
+                            </div>
+                          </div>
+              
+                        </div>
+                        <!-- /SECOND ROW -->
+                      </div>
+                    </div>
+                  </div>
+                  <!-- /COLUMNS -->
+                </div>
+                <!-- /MADE BY -->
+              
+                <!-- CONTACT NOW -->
+                <div class="join-now module" id="joinnow">
+                  <div class="container">
+                    <div class="row">
+                      <!-- TITLE -->
+                      <div class="col-sm-8 col-sm-offset-2 ds-join-now-title">
+                        <h2>Punya ide atau semacamnya?</h2>
+                        <p>bla bla bla marketing bla bla bla marketing bla bla bla marketing bla bla bla marketing bla bla bla
+                          marketing.</p>
+                      </div>
+                      <!-- /TITLE -->
+                    </div>
+                    <div class="row">
+                      <!-- CONTACT NOW FORM -->
+                      <div class="col-sm-12 ds-join-now-form">
+                        <form id="join-now" class="clearfix">
+                          <input type="text" value="" name="join-now-name" id="join-now-name" placeholder="Nama Panggilan">
+                          <!--
+                            --><input type="text" value="" name="join-now-email" id="join-now-email" placeholder="Email">
+                          <!--
+                            --><input type="submit" value="KASIH TAU" name="join-now-submit">
+                        </form>
+              
+                        <!-- CONTACT NOW FORM MESSAGE -->
+                        <div class="ds-join-now-message">
+              
+                        </div>
+                        <!-- /CONTACT NOW FORM MESSAGE -->
+                      </div>
+                      <!-- /CONTACT NOW FORM -->
+                    </div>
+                  </div>
+                </div>
+                <!-- /CONTACT NOW -->
+              
+                <!-- FOOTER -->
+                <div class="footer module">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-sm-12 ds-footer-copyrights">
+                        <p>© 2019. Dibuat oleh <a href="https://farridkuntoro.me">@Farrid_Jr</a> dan di kembangkan oleh
+                          <b>UHA-Developer</b>.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /FOOTER -->
+              
+                <!-- jQuery -->
+                <script src="/js/jquery-1.11.0.min.js"></script>
+                <!-- Backstretch -->
+                <script src="/js/jquery.backstretch.min.js"></script>
+                <!-- slick -->
+                <script src="/js/slick.min.js"></script>
+                <!-- Featherlight -->
+                <script src="/js/featherlight.min.js"></script>
+                <!-- WordsRotator -->
+                <script src="/js/jquery.wordrotator.min.js"></script>
+                <!-- Countdown -->
+                <script src="/uha/js/jquery.countdown.js"></script>
+                <!-- Smooth Scroll -->
+                <script src="/js/smoothscroll.js"></script>
+                <!-- Ajax mailchimp -->
+                <script src="/js/jquery.ajaxchimp.js"></script>
+                <!-- WOW.js -->
+                <script src="/js/wow.min.js"></script>
+                <!-- Masnoo js scripts -->
+                <script src="/js/ds-masnoo.js"></script>
+              
+              </body>
+</html>
